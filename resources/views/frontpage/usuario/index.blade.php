d8f795dca5cbbb8061b37de807745329f4ccc095
kjayru <wiltinoco@gmail.com>
2023-09-20 17:17:03 -0500

    fix

--- resources/views/frontpage/usuario/index.blade.php
+++ resources/views/frontpage/usuario/index.blade.php
@@ -31,7 +31,7 @@
 					<table class="todocursos__card__body_table table">
                         <tr>
                             <td class="titulo"><strong>User</strong>  </td>
-                            <td>{{@$user->name}}</td>
+                            <td>{{@$user->profile->firstname}}</td>
                         </tr>
                         <tr>
                             <td class="titulo"><strong>Email</strong>  </td>
