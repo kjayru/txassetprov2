4df63e804d090ecaa73682210d5eec52e9b8c098
kjayru <wiltinoco@gmail.com>
2023-10-11 15:39:37 -0500

    fix

--- resources/views/layouts/frontend/cursos/menu/quiz.blade.php
+++ resources/views/layouts/frontend/cursos/menu/quiz.blade.php
@@ -7,7 +7,7 @@
 <li class="encurso__temas__lista__item__sublista__item {{UserCourseChapter::userQuiz($user_course_id,$cont['contenidos']['capitulo_id'])?'finalizado':''}} {{ChapterQuiz::cursandoQuiz(@$chapter->slug,$cont['contenidos']['capitulo_id'],$quiz)?'active':''}}"  >
 
     @if(UserCourseChapter::userQuiz($user_course_id,$cont['contenidos']['capitulo_id']))
-    <a href="/learn/{{$cont['contenidos']['curso_slug']}}/{{$cont['contenidos']['capitulo_slug']}}/quiz/{{$cont['contenidos']['quiz_content']->chapter_id}}">Question about the chapter</a>
+    <a href="/learn/{{$user_course_id}}/{{$cont['contenidos']['curso_slug']}}/{{$cont['contenidos']['capitulo_slug']}}/quiz/{{$cont['contenidos']['quiz_content']->chapter_id}}">Question about the chapter</a>
     @else
     <a href="#" >Question about the chapter</a>
    @endif
